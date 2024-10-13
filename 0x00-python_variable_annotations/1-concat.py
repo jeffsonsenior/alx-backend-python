08f399ca9db476eb7f5fdd7d910e8adfc34ee280
jeffsonsenior <kinyuajeff@gmail.com>
2024-10-13 02:45:57 +0000

    correction

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 '''Task.
 '''
+
+
+
 def concat(str1: str, str2: str) -> str:
     '''Concatenates two strings.
     '''
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,6 +2,8 @@
 '''Task 2's.
 '''
 
+
+
 def floor(a: float) -> int:
     '''Computes the floor of a floating-point number.
     '''
